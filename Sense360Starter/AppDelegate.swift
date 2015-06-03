51a6f0c70b5094633dce1e82eb4ec578ccc46669
Kamil Mroczek <kamil.mroczek@gmail.com>
2015-06-02 22:17:55 -0700

    Added libraries

--- Sense360Starter/AppDelegate.swift
+++ Sense360Starter/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SenseSdk
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+
+        SenseSdk.enableSdkWithKey("hello")
+
         return true
     }
 
