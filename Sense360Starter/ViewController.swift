1d80c66291ddc150c9ad23b26a3ccc54a5603b87
Kamil Mroczek <kamil.mroczek@gmail.com>
2015-06-05 15:26:23 -0700

    Allowed testing restaurant trigger in Swift
    - Setup UI for Objective-C

--- Sense360Starter/ViewController.swift
+++ Sense360Starter/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import SenseSdk
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var triggerButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func triggerPressed(sender: UIButton) {
+        //Create a fake restaurant
+        let place = PoiPlace(latitude: 34.111, longitude: -118.111, radius: 50, name: "Big Restaurant", id: "id1", types: [.Restaurant])
 
+        let errorPointer = SenseSdkErrorPointer.create()
+        // This method should only be used for testing
+        SenseSdkTestUtility.fireTrigger(
+            fromRecipe: "ArrivedAtRestaurant",
+            confidenceLevel: ConfidenceLevel.Medium,
+            places: [place],
+            errorPtr: errorPointer
+        )
+
+        if errorPointer.error != nil {
+            NotificationSender.send("Error sending trigger")
+        }
+    }
 }
 
